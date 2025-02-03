e27659f9fc8fb78ee67f5ecab4256c42065f3801
merwil vegas <com2pa@gmail.com>
2025-02-03 09:24:53 -0400

    se modifico la ruta de las paginas

--- src/router/Root.jsx
+++ src/router/Root.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Project from '../pages/Project';
-import Resumen from '../pages/Resumen';
+import Home from '../page/Home';
+import About from '../page/About';
+import Project from '../page/Project';
+import Resumen from '../page/Resumen';
 
 export const Root = () => {
   return (
