5c537bfc08499f303902bc127e4c76f3f8d062ad
merwil vegas <com2pa@gmail.com>
2025-02-05 18:36:28 -0400

    cambio hashRouter

--- src/router/Root.jsx
+++ src/router/Root.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter,Route, Routes } from 'react-router-dom';
+import {HashRouter as Router ,Route, Routes } from 'react-router-dom';
 import Home from '../page/Home';
 import About from '../page/About';
 import Project from '../page/Project';
@@ -6,7 +6,7 @@ import Resumen from '../page/Resumen';
 
 export const Root = () => {
   return (
-    <BrowserRouter >
+    <Router >
       <Routes>
         <Route
           path='/'
@@ -26,7 +26,7 @@ export const Root = () => {
           element={<Resumen />}
         />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
